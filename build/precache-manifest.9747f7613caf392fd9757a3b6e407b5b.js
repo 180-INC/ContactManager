585fc1a9ac8f90d5eb89dceadc1adb44505bf520
Adeoluwa Agbakosi <84202664+180-INC@users.noreply.github.com>
2021-11-04 20:30:58 +0100

    Add files via upload

---
+++ build/precache-manifest.9747f7613caf392fd9757a3b6e407b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed5773a29578e180396dc90aa9b3622",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "5e04cce1a9c9db564a6c",
+    "url": "/ContactManager/static/css/main.d1902e6c.chunk.css"
+  },
+  {
+    "revision": "01f488b5a7ad559648b7",
+    "url": "/ContactManager/static/js/2.70712732.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/ContactManager/static/js/2.70712732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e04cce1a9c9db564a6c",
+    "url": "/ContactManager/static/js/main.33f70007.chunk.js"
+  },
+  {
+    "revision": "1f4ec5adc841838e5952",
+    "url": "/ContactManager/static/js/runtime-main.bbf0b07a.js"
+  }
+]);
